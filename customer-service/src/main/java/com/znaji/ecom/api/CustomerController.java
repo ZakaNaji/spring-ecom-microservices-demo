e9b94ff800fe8f21517c30e9b13c57e64d95d1f4
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-01-16 16:42:42 +0100

    customer controller

--- customer-service/src/main/java/com/znaji/ecom/api/CustomerController.java
+++ customer-service/src/main/java/com/znaji/ecom/api/CustomerController.java
@@ -1,13 +1,33 @@
 package com.znaji.ecom.api;
 
+import com.znaji.ecom.dto.CreateCustomerRequest;
+import com.znaji.ecom.dto.CustomerResponse;
+import com.znaji.ecom.service.CustomerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
 
-    @GetMapping("/customers/ping")
-    public String ping() {
-        return "customer-service up";
+    private final CustomerService service;
+
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerResponse create(@Valid @RequestBody CreateCustomerRequest request) {
+        return service.create(request);
+    }
+
+    @GetMapping("/{id}")
+    public CustomerResponse getById(@PathVariable String id) {
+        return service.getById(id);
     }
 }
