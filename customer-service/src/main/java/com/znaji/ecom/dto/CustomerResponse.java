f23fe35fa570e94f1087e1bba09cee82cb334ff2
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-01-16 16:43:25 +0100

    customer model & dto

---
+++ customer-service/src/main/java/com/znaji/ecom/dto/CustomerResponse.java
@@ -0,0 +1,9 @@
+package com.znaji.ecom.dto;
+
+public record CustomerResponse(
+        String id,
+        String firstName,
+        String lastName,
+        String email,
+        String phone
+) {}
