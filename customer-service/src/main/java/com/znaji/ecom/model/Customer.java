67c8805dc2ed2a30af0449d12ea96b4aa6abec8a
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-01-16 16:43:18 +0100

    customer model & dto

---
+++ customer-service/src/main/java/com/znaji/ecom/model/Customer.java
@@ -0,0 +1,27 @@
+package com.znaji.ecom.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Document("customers")
+public class Customer {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String phone;
+}
