3f8e3d10fefff8a736b18201bc775fec87bf0865
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-01-17 15:11:12 +0100

    feat: custom exceptions

---
+++ customer-service/src/main/java/com/znaji/ecom/exception/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.znaji.ecom.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+    public CustomerNotFoundException(String id) {
+        super("Customer not found: " + id);
+    }
+}
