d095de9d5f16af20a02fb3750f5e1351142d58e6
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-01-19 16:27:25 +0100

    feat: custom exception

---
+++ product-service/src/main/java/com/znaji/ecom/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.znaji.ecom.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(Long id) {
+        super("Product not found: " + id);
+    }
+}
